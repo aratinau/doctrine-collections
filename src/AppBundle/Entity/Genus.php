e4db6e4bcfd1ff088bfa7950e7f46af4d194cfc8
Aymeric <hello@codeparadise.xyz>
2020-10-10 11:57:34 +0200

    Inserting into a ManyToMany

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -176,4 +176,14 @@ class Genus
     {
         $this->slug = $slug;
     }
+
+    public function addGenusScientist(User $user)
+    {
+        // avoid duplicate
+        if ($this->genusScientists->contains($user)) {
+            return;
+        }
+
+        $this->genusScientists[] = $user;
+    }
 }

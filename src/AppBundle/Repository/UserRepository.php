4cc5845bb47e064a27df7eb5affabd5f14fd379d
Aymeric <hello@codeparadise.xyz>
2020-10-22 20:00:13 +0200

    EntityType Checkboxes with ManyToMany

--- src/AppBundle/Repository/UserRepository.php
+++ src/AppBundle/Repository/UserRepository.php
@@ -6,5 +6,12 @@ use Doctrine\ORM\EntityRepository;
 
 class UserRepository extends EntityRepository
 {
-
+    public function createIsScientistQueryBuilder()
+    {
+        // Attention: c'est ça retourne uniquement le query builder
+        return $this->createQueryBuilder('user')
+            ->andWhere('user.isScientist = :isScientist')
+            ->setParameter('isScientist', true)
+        ;
+    }
 }

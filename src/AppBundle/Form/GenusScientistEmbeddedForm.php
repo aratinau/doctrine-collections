ed7b18f8f80ff9a4740733689b1ddf8711b7fba6
Aymeric <hello@codeparadise.xyz>
2020-10-23 19:28:47 +0200

    Form Events: A readonly Embedded Field

--- src/AppBundle/Form/GenusScientistEmbeddedForm.php
+++ src/AppBundle/Form/GenusScientistEmbeddedForm.php
@@ -8,6 +8,8 @@ use AppBundle\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class GenusScientistEmbeddedForm extends AbstractType
@@ -23,7 +25,21 @@ class GenusScientistEmbeddedForm extends AbstractType
                 }
             ])
             ->add('yearsStudied')
+            ->addEventListener(
+                FormEvents::POST_SET_DATA,
+                array($this, 'onPostSetData')
+            )
         ;
+    }
+
+    public function onPostSetData(FormEvent $event)
+    {
+        // on regarde s'il y a un Id, si oui -> c'est une modification, donc le cas qu'on cherche
+        if ($event->getData() && $event->getData()->getId())
+        {
+            $form = $event->getForm();
+            unset($form['user']);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
